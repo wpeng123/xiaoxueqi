3c57ef5e5250aa0730d7996b55b12495ba19e517
Henry <80731107+HengWang20001029@users.noreply.github.com>
2022-07-06 13:55:14 +0800

    fix_ai_1.0

--- Assets/scripts/Ai_Move_main.cs
+++ Assets/scripts/Ai_Move_main.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum Situation
 {
-    chase, attack, dead, normal ,retreat
+    chase, attack, dead, normal, retreat
 }
 public struct Ai_variable
 {//Ai脚本需要的两个数值变量
@@ -42,17 +42,21 @@ public class Ai_Move_main : MonoBehaviour
     public Rigidbody2D target;
     public int counter;//用于记录球体运动情况 被卡住time
 
+    public float nearest_attack_distance;
+    public float farthest_attack_distance;
+
     public float bulletspeed;
     public float bulletscale;
     public float bulletDeathTime;
     public float bulletAmount;
     public float bulletFrequency;
 
-    public int attack_interval_time;
+    public float attack_interval_time;
 
     private int is_attack;//是否攻击
     private Transform attack1;
     private Transform attack2;
+
 
     void set_attack()
     {
@@ -61,20 +65,58 @@ public class Ai_Move_main : MonoBehaviour
     void initialie()
     {
         is_attack = 1;
-        if(attack_interval_time==0)
+        if (attack_interval_time == 0)
         {
             attack_interval_time = 4;
         }
-        attack1 = GetChild(this.transform,"BulletSpawner");
-        attack2 = GetChild(this.transform, "BulletSpawner (1)");
+
         if (enemy.tag == "enemy_shooter")
         {
-            enemy_stru.attack_distance_0 = 2;
-            enemy_stru.attack_distance_1 = 4;
+            attack1 = GetChild(this.transform, "BulletSpawner");
+            attack2 = GetChild(this.transform, "BulletSpawner (1)");
+            if (nearest_attack_distance != 0)
+            {
+                enemy_stru.attack_distance_0 = nearest_attack_distance;
+            }
+            else
+            {
+                enemy_stru.attack_distance_0 = 4;
+            }
+            if (farthest_attack_distance != 0)
+            {
+                enemy_stru.attack_distance_1 = farthest_attack_distance;
+            }
+            else
+            {
+                enemy_stru.attack_distance_1 = 10;
+            }
+
+            enemy_stru.distance_apart = enemy_stru.attack_distance_0;
+        }
+        else if (enemy.tag == "enemy_spider")
+        {
+            attack1 = GetChild(enemy.transform, "spawner");
+
+            if (nearest_attack_distance != 0)
+            {
+                enemy_stru.attack_distance_0 = nearest_attack_distance;
+            }
+            else
+            {
+                enemy_stru.attack_distance_0 = 4;
+            }
+            if (farthest_attack_distance != 0)
+            {
+                enemy_stru.attack_distance_1 = farthest_attack_distance;
+            }
+            else
+            {
+                enemy_stru.attack_distance_1 = 10;
+            }
             enemy_stru.distance_apart = enemy_stru.attack_distance_0;
         }
         else
-        enemy_stru.distance_apart = 0.1f;//enemy需要与target之间保持的距离
+            enemy_stru.distance_apart = 0.1f;//enemy需要与target之间保持的距离
 
         //不同距离移动速度的权值
         weight.stage_03 = 10.70f;
@@ -86,22 +128,35 @@ public class Ai_Move_main : MonoBehaviour
 
 
         enemy_stru.situation = Situation.chase;//当前的情况
+        if (enemy.tag == "enemy_shooter")
+        {
+            TowerManager machineGunManager = attack1.GetComponent<TowerManager>();
+            machineGunManager.amount = bulletAmount;
+            machineGunManager.bulletDeathTime = bulletDeathTime;
+            machineGunManager.spawnfrequency = bulletFrequency;
+            machineGunManager.bulletscale = bulletscale;
+            machineGunManager.bulletspeed = bulletspeed;
+            machineGunManager.rotationSpeed = 0;
 
-        TowerManager machineGunManager = attack1.GetComponent<TowerManager>();
-        machineGunManager.amount = bulletAmount;
-        machineGunManager.bulletDeathTime = bulletDeathTime;
-        machineGunManager.spawnfrequency = bulletFrequency;
-        machineGunManager.bulletscale = bulletscale;
-        machineGunManager.bulletspeed = bulletspeed;
-        machineGunManager.rotationSpeed = 0;
+            TowerManager machineGunManager2 = attack2.GetComponent<TowerManager>();
+            machineGunManager2.amount = bulletAmount;
+            machineGunManager2.bulletDeathTime = bulletDeathTime;
+            machineGunManager2.spawnfrequency = bulletFrequency;
+            machineGunManager2.bulletscale = bulletscale;
+            machineGunManager2.bulletspeed = bulletspeed;
+            machineGunManager2.rotationSpeed = 0;
+        }
+        else if (enemy.tag == "enemy_spider")
+        {
+            TowerManager machineGunManager = attack1.GetComponent<TowerManager>();
+            machineGunManager.amount = bulletAmount;
+            machineGunManager.bulletDeathTime = bulletDeathTime;
+            machineGunManager.spawnfrequency = bulletFrequency;
+            machineGunManager.bulletscale = bulletscale;
+            machineGunManager.bulletspeed = bulletspeed;
 
-        TowerManager machineGunManager2 = attack2.GetComponent<TowerManager>();
-        machineGunManager2.amount = bulletAmount;
-        machineGunManager2.bulletDeathTime = bulletDeathTime;
-        machineGunManager2.spawnfrequency = bulletFrequency;
-        machineGunManager2.bulletscale = bulletscale;
-        machineGunManager2.bulletspeed = bulletspeed;
-        machineGunManager2.rotationSpeed = 0;
+        }
+
 
 
     }
@@ -109,75 +164,103 @@ public class Ai_Move_main : MonoBehaviour
     {
 
 
-        if(is_attack==1)
+        if (is_attack == 1)
         {
-            Debug.Log("攻击！！");
+           // Debug.Log(enemy.tag+enemy.tag + " 攻击！！");
             enemy.velocity = Vector2.zero;
-            TowerManager attack3 = attack1.GetComponent<TowerManager>();
-            TowerManager attack4 = attack2.GetComponent<TowerManager>();
-            attack3.Spawn3();
-            attack4.Spawn3();
+            if (enemy.tag == "enemy_shooter")
+            {
+                Animator spider_chase = GetChild(enemy.transform, "ArmouredCarShoot1").GetComponent<Animator>();
+                spider_chase.SetBool("Shoot", true);
+                TowerManager attack3 = attack1.GetComponent<TowerManager>();
+                TowerManager attack4 = attack2.GetComponent<TowerManager>();
+                attack3.Spawn3();
+                attack4.Spawn3();
+            }
+            else if (enemy.tag == "enemy_spider")
+            {
+                Animator spider_chase = GetChild(enemy.transform, "TurretShoot1").GetComponent<Animator>();
+                spider_chase.SetBool("Shoot", true);
+                //Debug.Log(this.tag + "attack");
+                TowerManager attack3 = attack1.GetComponent<TowerManager>();
+                attack3.Spawn3();
+            }
+           
+
+
             is_attack = -1;
         }
-        else if(is_attack==-1)
+        else if (is_attack == -1)
         {
             is_attack = 0;
+            if (enemy.tag == "enemy_shooter")
+            {
+                Animator spider_chase = GetChild(enemy.transform, "ArmouredCarShoot1").GetComponent<Animator>();
+                spider_chase.SetBool("Shoot", false);
+              
+            }
+            else if (enemy.tag == "enemy_spider")
+            {
+                Animator spider_chase = GetChild(enemy.transform, "TurretShoot1").GetComponent<Animator>();
+                spider_chase.SetBool("Shoot", false);
+            }
+         
             Invoke("set_attack", attack_interval_time);
         }
-       
+
     }
     Situation get_situation()
-    {      
-        if (enemy.tag == "enemy_shooter")
+    {
+        if (enemy.tag == "enemy_shooter" || enemy.tag == "enemy_spider")
         {
-            if (enemy_stru.distance>enemy_stru.attack_distance_1)
-            {            
+            if (enemy_stru.distance > enemy_stru.attack_distance_1)
+            {
                 return Situation.chase;
-               
+
 
             }
-            else if(enemy_stru.distance < enemy_stru.attack_distance_1&& enemy_stru.distance >enemy_stru.attack_distance_0)
-            {          
+            else if (enemy_stru.distance < enemy_stru.attack_distance_1 && enemy_stru.distance > enemy_stru.attack_distance_0)
+            {
 
                 return Situation.attack;
-             
+
             }
             else
-            {            
+            {
                 return Situation.retreat;
-              
+
             }
         }
         else
         {
-           /*if (enemy_stru.distance > position_stru.distance_apart)
-            {
-                // enemy.AddRelativeForce(getforce());
-                if (position_stru.last_distance < position_stru.distance + 0.05 && position_stru.last_distance > position_stru.distance - 0.05)
-                    counter++;
-                if (counter >= 4)
-                {
-                    if (situation != Situation.normal)
-                    {
-                        situation = Situation.normal;
-                        counter = 0;
-                    }
+            /*if (enemy_stru.distance > position_stru.distance_apart)
+             {
+                 // enemy.AddRelativeForce(getforce());
+                 if (position_stru.last_distance < position_stru.distance + 0.05 && position_stru.last_distance > position_stru.distance - 0.05)
+                     counter++;
+                 if (counter >= 4)
+                 {
+                     if (situation != Situation.normal)
+                     {
+                         situation = Situation.normal;
+                         counter = 0;
+                     }
 
 
-                    //if()
-                }
+                     //if()
+                 }
 
-                //enemy.AddForce(getforce());
-                //print("add force" + getforce());
-            }
-            else
-            {
-                enemy.velocity = Vector3.zero;
-                //print("stop");
-            }*/
+                 //enemy.AddForce(getforce());
+                 //print("add force" + getforce());
+             }
+             else
+             {
+                 enemy.velocity = Vector3.zero;
+                 //print("stop");
+             }*/
         }
         return Situation.normal;
-        
+
     }
 
     bool need_get_situation()
@@ -254,7 +337,7 @@ public class Ai_Move_main : MonoBehaviour
         direction.y = scale_dir_y;
 
         //Debug.Log(direction);
-        if(proportion_x>proportion_y)
+        if (proportion_x > proportion_y)
         {
             direction.y = 0;
         }
@@ -276,6 +359,9 @@ public class Ai_Move_main : MonoBehaviour
 
     void update_information()
     {
+        nearest_attack_distance = enemy_stru.attack_distance_0;
+        farthest_attack_distance = enemy_stru.attack_distance_1;
+
         target_stru.position_x = target.transform.position.x;
         target_stru.position_y = target.transform.position.y;
         target_stru.position_z = target.transform.position.z;
@@ -288,9 +374,13 @@ public class Ai_Move_main : MonoBehaviour
         //LookAt2D(target.position);
 
         enemy_stru.distance = get_distance();
-        //if(need_get_situation())
-        enemy_stru.situation =  get_situation();
-       // Debug.Log("position_stru.position_x" + position_stru.position_x + " position_stru.position_y" + position_stru.position_y + "position_stru.diatance" + position_stru.distance);
+        if (need_get_situation() && enemy.tag == "enemy_spider")
+            enemy_stru.situation = get_situation();
+        else
+        {
+            enemy_stru.situation = get_situation();
+        }
+        // Debug.Log("position_stru.position_x" + position_stru.position_x + " position_stru.position_y" + position_stru.position_y + "position_stru.diatance" + position_stru.distance);
         //Debug.Log("position_stru.diatance" + enemy_stru.distance);
     }
     void Start()
@@ -306,24 +396,46 @@ public class Ai_Move_main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // Debug.Log(enemy.tag);
+
         update_information();
         switch (enemy_stru.situation)
         {
             case Situation.chase:
                 {
-                    if(enemy.tag!="enemy_shooter")
-                    enemy.velocity = getforce_shooter();
-                    else
+
+
+                    if (enemy.tag == "enemy_spider")
                     {
-            
+                        Animator spider_chase = GetChild(enemy.transform, "TurretWalk1").GetComponent<Animator>();
+                        spider_chase.SetBool("Walk", true);
+
+                        enemy.velocity = getforce_shooter();
+                    }
+                    else if (enemy.tag == "enemy_shooter")
+                    {
+                        Animator spider_chase = GetChild(enemy.transform, "ArmouredCarRun1").GetComponent<Animator>();
+                        spider_chase.SetBool("Run", true);
                         enemy.velocity = getforce();
                     }
+                    //Debug.Log(enemy.tag + enemy.velocity);
                     break;
                 }
             case Situation.attack:
                 {
-                   // Debug.Log("attack");
+                    if (enemy.tag == "enemy_spider")
+                    {
+                        Animator spider_chase = GetChild(enemy.transform, "TurretWalk1").GetComponent<Animator>();
+                        spider_chase.SetBool("Walk", false);
+
+                       
+                    }
+                    else if (enemy.tag == "enemy_shooter")
+                    {
+                        Animator spider_chase = GetChild(enemy.transform, "ArmouredCarRun1").GetComponent<Animator>();
+                        spider_chase.SetBool("Run", false);
+                        
+                    }
+                  
                     attack();
                     //
                     break;
@@ -334,45 +446,51 @@ public class Ai_Move_main : MonoBehaviour
                     break;
                 }
             case Situation.retreat:
-                {
-                    enemy.velocity = getforce_shooter()*-1;
+                {                
+                    enemy.velocity = getforce_shooter() * (-0.5f);
                     break;
                 }
-               
+
 
         }
+        //enemy.velocity = Vector2.zero;
+        if (enemy.tag == "enemy_shooter")
+        {
+            TowerManager EmitterManager1 = attack1.GetComponent<TowerManager>();
+            TowerManager EmitterManager2 = attack2.GetComponent<TowerManager>();
+            EmitterManager1.defaultZ = transform.eulerAngles.z;
+            EmitterManager2.defaultZ = transform.eulerAngles.z;
 
-        TowerManager EmitterManager1 = attack1.GetComponent<TowerManager>();
-        TowerManager EmitterManager2 = attack2.GetComponent<TowerManager>();
-        EmitterManager1.defaultZ = transform.eulerAngles.z;
-        EmitterManager2.defaultZ = transform.eulerAngles.z;
-
+        }
+        else if (enemy.tag == "enemu_spider")
+        {
+            TowerManager EmitterManager1 = attack1.GetComponent<TowerManager>();
+            EmitterManager1.defaultZ = transform.eulerAngles.z;
+        }
     }
-    public static Transform GetChild(Transform parentTF, string childName)
-    {
-        //在子物体中查找
-        Transform childTF = parentTF.Find(childName);
+        
 
-        if (childTF != null)
+        public static Transform GetChild(Transform parentTF, string childName)
         {
-            return childTF;
-        }
-        //将问题交由子物体
-        int count = parentTF.childCount;
-        for (int i = 0; i < count; i++)
-        {
-            childTF = GetChild(parentTF.GetChild(i), childName);
+            //在子物体中查找
+            Transform childTF = parentTF.Find(childName);
+
             if (childTF != null)
             {
                 return childTF;
             }
+            //将问题交由子物体
+            int count = parentTF.childCount;
+            for (int i = 0; i < count; i++)
+            {
+                childTF = GetChild(parentTF.GetChild(i), childName);
+                if (childTF != null)
+                {
+                    return childTF;
+                }
+            }
+            return null;
         }
-        return null;
-    }
-    //void LookAt2D(Vector2 dir)
-    //{
-    //    Debug.Log("look at");
-    //    float angle = Mathf.Atan2(-dir.x, dir.y) * Mathf.Rad2Deg;
-    //    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    //}
+
+    
 }
